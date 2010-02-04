8d5ecb5db0a37e960b6c58f4f53e6870ccb2cc40
chrv <chrv@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-02-04 21:02:14 +0000

    new spi mapping and joystick support

--- avr/current/joystick.c
+++ avr/current/joystick.c
@@ -4,24 +4,21 @@
 #include "mytypes.h"
 
 #include "rs232.h"
+#include "zx.h"
 #include "joystick.h"
 
 void joystick_task(void)
 {
 	static UBYTE joy_state = 0;
-	UBYTE temp = JOYSTICK_PIN;
+	UBYTE temp = (~JOYSTICK_PIN) & JOYSTICK_MASK;
 
-	if ( (joy_state ^ temp) & JOYSTICK_MASK )
+	if ( joy_state ^ temp )
 	{
 		//change state of joystick pins
-		joy_state = temp & JOYSTICK_MASK;
+		joy_state = temp;
 
-	   //calculate content of kempston joysticks port
-	   temp = ((joy_state & (1<<JOYSTICK_RIGHT))? 0: KEMPSTON_JOYSTICK_BIT_RIGHT ) +
-			  ((joy_state & (1<<JOYSTICK_LEFT))? 0: KEMPSTON_JOYSTICK_BIT_LEFT ) +
-			  ((joy_state & (1<<JOYSTICK_UP))? 0: KEMPSTON_JOYSTICK_BIT_UP ) +
-			  ((joy_state & (1<<JOYSTICK_DOWN))? 0: KEMPSTON_JOYSTICK_BIT_DOWN )+
-			  ((joy_state & (1<<JOYSTICK_FIRE))? 0: KEMPSTON_JOYSTICK_BIT_FIRE );
+		//send to port
+		zx_spi_send(SPI_KEMPSTON_JOYSTICK, joy_state, 0x7F);
 
 #ifdef LOGENABLE
 	char log_joystick[] = "JS..\r\n";
@@ -29,8 +26,5 @@ void joystick_task(void)
 	log_joystick[3] = ((temp & 0x0F) <= 9 )?'0'+(temp & 0x0F):'A'+(temp & 0x0F)-10;
 	to_log(log_joystick);
 #endif
-
-		//send to port
-		//zx_spi_send(addr, temp, 0x7F);
 	}
 }
