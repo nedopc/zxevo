efa32151b13908f97ff02b7fecff99b0f977f822
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-02-24 14:06:32 +0000

    Добавил для теста Рейдж, обновил версию сервиса до 0.13 (добавил поддержку SMUC)

--- rom/page0/Source/make_main.bat
+++ rom/page0/Source/make_main.bat
@@ -2,7 +2,7 @@
 
 rem тестовая сборка основного куска сервиса
 
-make_micro_boot_fat.bat
+..\..\..\tools\sjasmplus\sjasmplus --sym=sym.log --lst=dump.log -isrc make_micro_boot_fat.a80
 
 ..\..\..\tools\sjasmplus\sjasmplus --sym=sym.log --lst=dump.log -isrc make_main.a80
 ..\..\..\tools\mhmt\mhmt -mlz main.rom main_pack.rom
