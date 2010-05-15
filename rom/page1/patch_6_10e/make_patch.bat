9581cde793ce52d4e1e8aef2f62f94b559b1bbef
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-05-15 16:43:49 +0000

    Выложил патченный тырдос 6.10. Доку отдельную не создавал. Все в сорце отписал.

--- rom/page1/patch_6_10e/make_patch.bat
+++ rom/page1/patch_6_10e/make_patch.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 
-..\..\..\tools\sjasmplus\sjasmplus --sym=sym.log --lst=dump.log -isrc make_patch.a80
+REM ..\..\..\tools\sjasmplus\sjasmplus --sym=sym.log --lst=dump.log -isrc make_patch.a80
+
+..\..\..\tools\sjasmplus\sjasmplus -isrc make_patch.a80
 
 rem ..\mhmt -mlz filename.rom filename_pack.rom
 
