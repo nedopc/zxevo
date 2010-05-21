c4665c418e99f57627ef1f10cded62b3a907666c
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-05-21 18:54:34 +0000

    Сваял версию НЕ ГЛЮК под номером 0.17. Серьезных изменений нет. Переписаны рестартовые кнопки, сделал версию для запуска без перекомпиляции и на пенте и на атм. Поправил драйвер винта. Дополнительными батниками можно собирать версии для режимов пента и атм и для сборки полной пзу на 512кил. Пропатченный тырдос присвоил номер 6.12е. Текстовый файлик положил рядом с сорцом патча.

--- rom/build_atm.bat
+++ rom/build_atm.bat
@@ -1,1 +1,1 @@
-copy /B /Y page3\*.rom+page1\dos6_10e_patch.rom+page2\*.rom+page0\servicesa.rom zxevo_atm.rom
+copy /B /Y page3\*.rom+page1\dos6_12e_patch.rom+page2\*.rom+page0\services.rom zxevo_atm.rom
