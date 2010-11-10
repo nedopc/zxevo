6990ba58a48fa24c60e2106f127628b08ba5dd60
ddp <ddp@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-11-10 16:03:58 +0000

    убрал лишнее

--- test/SD_LOGGER/AVR/clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if exist SDLOGGER.hex del /f /q SDLOGGER.hex
-if exist SDLOGGER.eep.hex del /f /q SDLOGGER.eep.hex
-if exist SDLOGGER.cof del /f /q SDLOGGER.cof
-if exist SDLOGGER.obj del /f /q SDLOGGER.obj
-if exist SDLOGGER.lst del /f /q SDLOGGER.lst
-if exist fpga.mlz del /f /q fpga.mlz
-if exist fpga.inc del /f /q fpga.inc
-if exist zxevo_fw.bin del /f /q zxevo_fw.bin
