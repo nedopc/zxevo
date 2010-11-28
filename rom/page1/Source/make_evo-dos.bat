7c9c7ba28cd15086a5b91bbb45e29f2acce3c88c
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-11-28 20:51:12 +0000

    Подправил кое-что по мелочи, изменил прошиватор. Доки поправил. Доки на еводос все еще нету. Еводос 0.13 и евосервис 0.24.

--- rom/page1/Source/make_evo-dos.bat
+++ rom/page1/Source/make_evo-dos.bat
@@ -1,8 +1,14 @@
 @ECHO OFF
 
-..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos.a80
-..\..\..\tools\asw\bin\p2bin evo-dos.p ..\evo-dos.rom -r $-$ -k
+..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_emu3d13.a80
+..\..\..\tools\asw\bin\p2bin evo-dos_emu3d13.p ..\evo-dos_emu3d13.rom -r $-$ -k
 
-copy /B /Y ..\evo-dos.rom d:\unrealspeccy\
+rem ..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_real.a80
+rem ..\..\..\tools\asw\bin\p2bin evo-dos_real.p ..\evo-dos_real.rom -r $-$ -k
+
+rem ..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_virt.a80
+rem ..\..\..\tools\asw\bin\p2bin evo-dos_virt.p ..\evo-dos_virt.rom -r $-$ -k
+
+copy /B /Y ..\evo-dos_real.rom d:\unrealspeccy\
 
 pause
