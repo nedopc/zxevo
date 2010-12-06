24cefa0620915122f190423bbf97dbec8e33b6d9
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2010-12-06 21:27:07 +0000

    В связи с воплями о не работе тех или иных программ (в основном игр) сделал некоторые исправления.

--- rom/page1/Source/make_evo-dos.bat
+++ rom/page1/Source/make_evo-dos.bat
@@ -3,12 +3,12 @@
 ..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_emu3d13.a80
 ..\..\..\tools\asw\bin\p2bin evo-dos_emu3d13.p ..\evo-dos_emu3d13.rom -r $-$ -k
 
-rem ..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_real.a80
-rem ..\..\..\tools\asw\bin\p2bin evo-dos_real.p ..\evo-dos_real.rom -r $-$ -k
+..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_virt.a80
+..\..\..\tools\asw\bin\p2bin evo-dos_virt.p ..\evo-dos_virt.rom -r $-$ -k
 
-rem ..\..\..\tools\asw\bin\asw -cpu z80undoc -L evo-dos_virt.a80
-rem ..\..\..\tools\asw\bin\p2bin evo-dos_virt.p ..\evo-dos_virt.rom -r $-$ -k
+rem copy /B /Y ..\evo-dos_emu3d13.rom d:\unrealspeccy\evo-dos.rom
+copy /B /Y ..\evo-dos_virt.rom d:\unrealspeccy\evo-dos.rom
 
-copy /B /Y ..\evo-dos_real.rom d:\unrealspeccy\
+REM ..\..\..\tools\mhmt\mhmt -mlz ..\evo-dos_emu3d13.rom evo-dos_emu3d13_pack.rom
 
 pause
