4bbeaab02cd432591bb0afca7dfdce02c9a50387
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2011-02-12 08:16:05 +0000

    Версии евосервис/еводос 032/021. Исправление ошибок.

--- rom/build_full.bat
+++ rom/build_full.bat
@@ -1,4 +1,4 @@
 
-copy /B /Y page0\services.rom+page1\dos6_12e_patch.rom+page2\*.rom+page3\*.rom+ff_64k.rom+ff_64k.rom+ff_64k.rom+ff_64k.rom+ff_64k.rom+page3\*.rom+page1\evo-dos_virt.rom+page2\*.rom+page0\services.rom+page3\*.rom+page1\evo-dos_emu3d13.rom+page2\*.rom+page0\services.rom zxevo_full.rom
+copy /B /Y page0\services.rom+page1\dos6_12e_patch.rom+page2\128_low.rom+page3\*.rom+ff_64k.rom+ff_64k.rom+ff_64k.rom+ff_64k.rom+ff_64k.rom+page3\*.rom+page1\evo-dos_virt.rom+page2\128_low.rom+page0\services.rom+page3\*.rom+page1\evo-dos_emu3d13.rom+page2\128_low.rom+page0\services.rom zxevo_full.rom
 
 pause
