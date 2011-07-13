183df1efca2bec2bb200b8984dfd4a2a3c412dcf
lvd <lvd2@users.noreply.github.com>
2011-07-13 20:27:13 +0000

    fpga/z14: fixed sdcard for 14mhz

--- fpga/z14/top.v
+++ fpga/z14/top.v
@@ -317,7 +317,7 @@ module top(
 	(
 		.fclk(fclk), .rst_n(rst_n), .zclk(zclk), .rfsh_n(rfsh_n), .zclk_out(clkz_out),
 		.zpos(zpos), .zneg(zneg),
-		.turbo( 2'b10/*{1'b0,~(peff7[4])}*/ ), .pre_cend(pre_cend), .cbeg(cbeg),
+		.turbo( {~(peff7[4]),(peff7[4])} ), .pre_cend(pre_cend), .cbeg(cbeg),
 		.zclk_stall( cpu_stall | (|zclk_stall) ), .int_turbo(int_turbo),
 		.external_port(external_port), .iorq_n(iorq_n), .m1_n(m1_n)
 	);
