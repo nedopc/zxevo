03a0cad646bfd92dbb5dfe1c3f895c97bf94f04e
dimkam <dimkam@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2012-02-19 09:52:27 +0000

    не сбрасывался бит переполнения фифо-приёма уарта. Извиняюсь, что влез без разрешения. протестил, бит стал сбрасыватся.

--- avr/current/rs232.c
+++ avr/current/rs232.c
@@ -204,7 +204,7 @@ void rs232_zx_write(UBYTE index, UBYTE data)
 				//receive FIFO reset
 				rs232_FI_start = rs232_FI_end = 0;
 				//set empty FIFO flag
-				rs232_LSR &= ~(0x01);
+				rs232_LSR &= ~(0x03);
 			}
 			if( data&(1<<2) )
 			{
