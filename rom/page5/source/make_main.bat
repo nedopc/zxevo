2512d668ec47d53cb2f2f44a3649d4a888bbf8a7
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2012-05-27 08:57:24 +0000

    Версия 0.5. Изменения в доках

--- rom/page5/source/make_main.bat
+++ rom/page5/source/make_main.bat
@@ -1,6 +1,23 @@
 @ECHO OFF
 
+cd ..\..\fat\source
+
+..\..\..\tools\asw\bin\asw -cpu z80undoc -U -L make_micro_boot_fat.a80
+..\..\..\tools\asw\bin\p2bin make_micro_boot_fat.p micro_boot_fat.rom -r $-$ -k
+
+..\..\..\tools\mhmt\mhmt -mlz micro_boot_fat.rom micro_boot_fat_pack.rom
+
+cd ..\..\page5\source
+
+..\..\..\tools\mhmt\mhmt -mlz 8x8_ar.fnt 8x8_ar_pack.bin
+..\..\..\tools\mhmt\mhmt -mlz 866_code.fnt 866_code_pack.bin
+..\..\..\tools\mhmt\mhmt -mlz atm_code.fnt atm_code_pack.bin
+
 ..\..\..\tools\asw\bin\asw -cpu z80undoc -U -L rst8service.a80
 ..\..\..\tools\asw\bin\p2bin rst8service.p ..\rst8service.rom -r $-$ -k
+
+del 8x8_ar_pack.bin
+del 866_code_pack.bin
+del atm_code_pack.bin
 
 pause
