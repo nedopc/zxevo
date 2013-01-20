a5eb6556f8b59567ae5e5eb8552008c5e915e382
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2013-01-20 17:39:55 +0000

    Версия 0.52b исправлена ошибка обнуления секунд

--- rom/page0/Source/make_main.bat
+++ rom/page0/Source/make_main.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-..\..\..\tools\asw\bin\asw -cpu z80undoc -U -L main.a80
+..\..\..\tools\asw\bin\asw -U -L main.a80
 ..\..\..\tools\asw\bin\p2bin main.p main.rom -r $-$ -k
 
 ..\..\..\tools\mhmt\mhmt -mlz main.rom main_pack.rom
