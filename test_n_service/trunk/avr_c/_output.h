8e7d30e5f6054591ac28f2e094f287287dc78f62
ddp <ddp@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2013-04-14 14:20:28 +0000

    Uncomplete C version of Test&Service (1 of 2)

---
+++ test_n_service/trunk/avr_c/_output.h
@@ -0,0 +1,18 @@
+#ifndef _OUTPUT_H
+#define _OUTPUT_H
+
+#include "_types.h"
+
+void put_char(u8 ch);
+void print_hexhalf(u8 b);
+void print_hexbyte(u8 b);
+void print_hexbyte_for_dump(u8 b);
+void print_hexlong(u32 l);
+void put_char_for_dump(u8 ch);
+void print_dec99(u8 b);
+void print_dec16(u16 w);
+void print_msg(PGM_U8_P msg);
+void print_mlmsg(PGM_U8_P *mlmsg);
+void print_short_vers(void);
+
+#endif
---
+++ test_n_service/trunk/avr_c/readme.txt
@@ -0,0 +1,12 @@
+Не завершённый проект Си версии Test&Service
+The uncomplete C version of the Test&Service
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+C compiler      - WinAVR 20100110
+ATMEGA128 clock - 11.0592 MHz
+Charset         - CP866
+
+Прошивка собирается в .\bin\
+
+Не реализованы:
+"Диагностика SD/MMC"
+"Программирование Flash-ROM"
