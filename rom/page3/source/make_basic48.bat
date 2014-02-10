7b27d287150c1ecfd0c080bb6ae8a2171761bda7
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2014-02-10 17:49:32 +0000

    Мелкие исправления в evodos и ers. Добавлен загрузчик isdos в service menu. Свалил все доки и хистори в один документ. Старые доки за гегадобностью удалю позже.

--- rom/page3/source/make_basic48.bat
+++ rom/page3/source/make_basic48.bat
@@ -1,10 +1,7 @@
 
 @ECHO OFF
 
-REM ..\..\..\tools\asw\bin\asw -cpu z80undoc -U -L make_bas48_only.a80
-REM ..\..\..\tools\asw\bin\p2bin make_bas48_only.p ..\basic48_only.rom -r $-$ -k
-
-..\..\..\tools\asw\bin\asw -cpu z80undoc -U -L make_bas48_128.a80
+..\..\..\tools\asw\bin\asw -U -L make_bas48_128.a80
 ..\..\..\tools\asw\bin\p2bin make_bas48_128.p ..\basic48.rom -r $-$ -k
 
 pause
