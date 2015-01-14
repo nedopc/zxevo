f7d2df48455a8dfc69c266ffa40b188043c0ebc4
savelij <savelij@5fac8f50-b818-07b6-3e01-e5304d2f9b6b>
2015-01-14 08:13:51 +0000

    ERS 0.56c Небольшое исправление и добавка дополнительных пзу.

--- rom/page5/source/make_main.bat
+++ rom/page5/source/make_main.bat
@@ -2,8 +2,8 @@
 
 cd ..\..\fat_boot\source
 
-..\..\..\tools\asw\bin\asw -U -L make_micro_boot_fat.a80
-..\..\..\tools\asw\bin\p2bin make_micro_boot_fat.p micro_boot_fat.rom -r $-$ -k
+..\..\..\tools\asw\bin\asw -U -L micro_boot_fat.a80
+..\..\..\tools\asw\bin\p2bin micro_boot_fat.p micro_boot_fat.rom -r $-$ -k
 
 ..\..\..\tools\mhmt\mhmt -mlz micro_boot_fat.rom micro_boot_fat_pack.rom
 
